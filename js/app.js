a64161fae44393ceb817af662369905b857cb636
Moonlie <rockmoonlie@gmail.com>
2023-11-08 15:00:12 +0100

    push final

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const apiKey = '923f8681e79e50c5cc3f1ff0a93a998d';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather'
 
 const ecrireLeNom = document.getElementById('country');
 const rechercheClick = document.getElementById('rechercher');
@@ -15,7 +15,7 @@ rechercheClick.addEventListener('click', () => {
 });
 
 function fetchWeather(leLieu) {
-    const url = `${apiUrl}?q=${leLieu}&appid=${apiKey}&units=metric`;
+    const url = `${apiUrl}?q=${leLieu}&appid=${apiKey}&units=metric&lang=fr`;
 
     fetch(url)
         .then(response => response.json())
